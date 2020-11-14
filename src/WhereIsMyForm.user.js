f6db50d9b752012bc9fd0a181b34119c223ed928
ForkKILLET <fork_killet@qq.com>
2020-11-15 04:06:01 +0800

    VER 0.3
    -! [win] Drag.
     : [win] is draggable now. The position will be storaged.
    -- [win] Several ordinary animations.
    -- [task] Task messages on the left of [win].
    -! [conf] A configuration panel.
     : [sc] Add shortcut zone.
    -- [mark] Marks can be toggled now.
    -$ Much stronger data wheel, currency `ts` and `ls`.
    -" Our stylesheet has passed the W3C CSS validate.
     : Use `transform: scale` instead of `zoom`.

--- src/WhereIsMyForm.user.js
+++ src/WhereIsMyForm.user.js
@@ -10,10 +10,9 @@
 // @grant        GM_addStyle
 // @grant        GM_getValue
 // @grant        GM_setValue
+// @grant        GM_listValues
 // @require      https://code.jquery.com/jquery-1.11.0.min.js
 // ==/UserScript==
-
-const $ = jQuery // Debug: Kill eslint warnings in TM editor: "'$' is not defined".
 
 function Throw(msg, detail) {
     msg = `[WIMF] ${msg}`
@@ -28,61 +27,56 @@ function Dat({ getter, setter, useWrapper, getW, setW, dataW }) {
             const p = path ? path + "." + n : n
             Object.defineProperty(src, n, useWrapper
                 ? {
-                    get: () => ls.__(".", getW(), p),
-                    set: v => {
-                        debugger
-                        const d = getW()
-                        ls.__(".", d)[p] = dataW ? dataW(v) : v
-                        setW(dataW ? dataW(d) : d)
-                    }
+                    get: () => dat._[p],
+                    set: v => dat._[p] = v
                 }
                 : {
                     get: () => getter(p, n),
-                    set: val => setter(p, n, val)
+                    set: v => setter(p, n, v)
                 }
             )
-            if (typeof opt[n] === "object") {
-                if (! dat[n]) dat[n] = {}
+            if (typeof opt[n] === "object" && ! Array.isArray(opt[n])) {
+                if (src[n] == null) src[n] = {}
                 dat(opt[n], dat[n], p)
             }
-            else if (! dat[n]) dat[n] = opt[n]
+            else if (src[n] == null) src[n] = opt[n]
         }
     }
 
-    function parse(path, spliter = ".", src = dat) {
-        const keys = path.split(spliter), len = keys.length
+    function parse(path, src = dat) {
+        const keys = path.split("."), len = keys.length
         function _parse(idx, now) {
             let k = keys[idx]
-            if (len - idx <= 1) return [ dat, k ]
+            if (len - idx <= 1) return [ now, k ]
             return _parse(idx + 1, now[k])
         }
         return _parse(0, src)
     }
 
-    dat.__ = (spliter, src, raw) => new Proxy(dat, {
+    dat._ = new Proxy(dat, {
         get: (_, path) => {
-            const r = parse(path, spliter, src)
-            return raw ? r : r[0][r[1]]
+            const r = parse(path, getW())
+            return r[0][r[1]]
         },
         set: (_, path, val) => {
-            const r = parse(path, spliter, src)
+            const d = getW(), r = parse(path, d)
             r[0][r[1]] = val
+            setW(dataW ? dataW(d) : d)
         }
     })
-    dat._ = dat.__()
 
     return dat
 }
 
 const ls = Dat({
     useWrapper: true,
-    getW: () => JSON.parse(unsafeWindow.localStorage.getItem("WIMF")),
+    getW: () => JSON.parse(unsafeWindow.localStorage.getItem("WIMF") ?? "{}"),
     setW: v => unsafeWindow.localStorage.setItem("WIMF", v),
     dataW: v => JSON.stringify(v)
 })
 const ts = Dat({
     useWrapper: true,
-    getW: () => GM_getValue("app"),
+    getW: () => GM_getValue("app") ?? {},
     setW: v => GM_setValue("app", v)
 })
 
@@ -114,10 +108,21 @@ $.fn.extend({
             if ($i.length) return $i
         }
         return null
+    },
+    melt(type, time, rm) {
+        if (type === "fadeio")
+            type = this.css("display") === "none" ? "fadein" : "fadeout"
+        if (type === "fadein") this.show()
+        this.css("animation", `melting-${type} ${time}s`)
+        time *= 1000
+        setTimeout(() => {
+            if (type !== "fadein") rm ? this.remove() : this.hide()
+        }, time > 100 ? time - 100 : time * 0.9)
+        // Note: A bit shorter than the animation duration for avoid "flash back".
     }
 })
 
-function scan({ hl, root, vOnly } = {
+function scan({ hl, root } = {
     root: "body"
 }) {
     const op = ls.op
@@ -127,25 +132,7 @@ function scan({ hl, root, vOnly } = {
           $c = $(`${root} input[type=checkbox],label`),
           $A = [ $t, $r, $c ]
 
-    if (vOnly) {
-        const vOf = [
-            $_ => $_.val(),
-            $_ => ($_.is("label") ? $_.forWhat() : $_).$attr
-        ]
-        let res = {}
-        for (let [ i, $i ] in $A.entries()) {
-            if (! $i.length) continue
-            const f = vOf[i]
-            if (f == null) Throw(`Can't get value. ${i}#`)
-            for (let j = 0; j < $i.length; j++) {
-                const $_ = $($i[j])
-                res[$_.path()] = f($_)
-            }
-        }
-        return res
-    }
-
-    $t.one("change.WIMF", () => {
+    $t.one("change.WIMF", function() {
         const $_ = $(this), path = $_.path(), val = $_.val()
         let f = true; for (let i in op) {
             if (op[i].type === "text" && op[i].path === path){
@@ -156,7 +143,7 @@ function scan({ hl, root, vOnly } = {
         if (f) op.push({ path, val, type: "text" })
         ls.op = op
     })
-    $r.one("click.WIMF", () => {
+    $r.one("click.WIMF", function() {
         let $_ = $(this)
         let path = $_.path(), label
         if ($_.is("label")) {
@@ -181,7 +168,7 @@ function scan({ hl, root, vOnly } = {
         if (f) op.push({ path, label, type: "radio" })
         ls.op = op
     })
-    $c.one("click.WIMF", () => {
+    $c.one("click.WIMF", function() {
         let $_ = $(this)
         let path = $_.path(), label
         if ($_.is("label")) {
@@ -200,6 +187,72 @@ function scan({ hl, root, vOnly } = {
     })
 
     if (typeof hl === "function") for (let $i of $A) hl($i)
+}
+
+function shortcut() {
+    let t_pk
+    const pk = []
+    pk.last = () => pk[pk.length - 1]
+
+    const $w = $(unsafeWindow), sc = ts.sc,
+          sc_rm = () => {
+              for (let i in sc) sc[i].m = 0
+          },
+          ct = () => {
+              clearTimeout(t_pk)
+              pk.splice(0)
+              pk.sdk =  false
+              t_pk = null
+              sc_rm()
+          },
+          st = () => {
+              clearTimeout(t_pk)
+              t_pk = setTimeout(ct, 800)
+          }
+
+    for (let i in sc) sc[i] = sc[i].split("&").map(i => i === "" ? sc.leader[0] : i)
+    const c_k = {
+        toggle: () => $(".WIMF").melt("fadeio", 1.5),
+        mark: UI.action.mark,
+        fill: UI.action.fill,
+        rset: UI.action.rset,
+        conf: UI.action.conf,
+        info: UI.action.info
+    }
+
+    ct()
+    $w.one("keydown.WIMF", e => {
+        st(); let ck = "", sdk = false
+        for (let dk of [ "alt", "ctrl", "shift", "meta" ]) {
+            if (e[dk + "Key"]) {
+                ck += dk = dk[0].toUpperCase() + dk.slice(1)
+                if (e.key === dk || e.key === "Control") {
+                    sdk = true; break
+                }
+                ck += "-"
+            }
+        }
+        if (! sdk) ck += e.key.toLowerCase()
+
+        if (pk.sdk && ck.includes(pk.last())) {
+            pk.pop()
+        }
+        pk.sdk = sdk
+        pk.push(ck)
+
+        for (let i in sc) {
+            const k = sc[i]
+            if (k.m === k.length) continue
+            if (k[k.m] === ck) {
+                if (++k.m === k.length) {
+                    if (i !== "leader") ct()
+                    if (c_k[i]) c_k[i]()
+                }
+            }
+            else if (pk.sdk && k[k.m].includes(ck)) ;
+            else k.m = 0
+        }
+    })
 }
 
 const UI = {}
@@ -226,6 +279,7 @@ UI.meta = {
         #{mainButton | quit 退出 | ❌}
     </div>
     <div class="WIMF-text"></div>
+    <div class="WIMF-task"></div>
 </div>
 `,
     testURL: "https://www.wjx.cn/newsurveys.aspx",
@@ -242,49 +296,91 @@ UI.meta = {
 <a href="#{testURL}">#{testURL}</a>
 `,
     confInput: (zone, name, hint) => `
-<input type="text" name="${zone}_${name}"
-    placeholder="${name[0].toUpperCase() + name.slice(1)} ${hint}"
-/>
+${name[0].toUpperCase() + name.slice(1)} ${hint}
+<input type="text" name="${zone}_${name}"/>
 `,
-    confApply: zone => `<button data-zone="${zone}">OK</button>`,
+    confApply: (zone) => `<button data-zone="${zone}">OK</button>`,
     conf: `
 <b class="WIMF-title">Configuration</b> <br/>
 
 <p>
-    Shortcuts 快捷键 <br/>
-    #{confInput | key | leader | 引导}
-    #{confInput | key | toggle | 开关浮窗}
-    #{confApply | key}
+    <b>Shortcuts 快捷键</b> <br/>
+    #{confInput | sc | leader | 引导}
+    #{confInput | sc | toggle | 开关浮窗}
+    #{confInput | sc | mark | 标记}
+    #{confInput | sc | fill | 填充}
+    #{confInput | sc | rset | 清存}
+    #{confInput | sc | conf | 设置}
+    #{confInput | sc | info | 关于}
+    #{confApply | sc}
 </p>
 `,
     styl: `
+/* :: Animation */
+
+@keyframes melting-sudden {
+    0%, 70% { opacity: 1; }
+    100% { opacity: 0; }
+}
+@keyframes melting-fadeout {
+    0% { opacity: 1; }
+    100% { opacity: 0; }
+}
+@keyframes melting-fadein {
+    0% { opacity: 0; }
+    100% { opacity: 1; }
+}
+
+/* :: Skeleton */
+
 .WIMF {
     position: fixed;
     z-index: 1919810;
+    user-select: none;
 
     opacity: 1;
     transition: top 1s, right 1s;
+    transform: scale(.9);
 }
 .WIMF, .WIMF * {
     box-sizing: content-box;
 }
-.WIMF-main, .WIMF-text {
-    position: absolute;
+
+.WIMF-main, .WIMF-text, .WIMF-task p {
+    width: 100px;
 
     padding: 0 3px 0 4.5px;
-
     border-radius: 12px;
     font-size: 12px;
     background-color: #fff;
     box-shadow: 0 0 4px #aaa;
 }
 .WIMF-main {
+    position: absolute;
     top: 0;
     right: 0;
-    width: 100px;
     height: 80px;
 }
-.WIMF-main::after {
+
+.WIMF-task {
+    position: absolute;
+    top: 0;
+    right: 115px;
+}
+
+/* :: Modification */
+
+.WIMF-mark {
+    background-color: #ffff81;
+}
+.WIMF-title {
+    display: block;
+    text-align: center;
+}
+
+/* :: Cell */
+
+.WIMF-main::after { /* Note: A cover. */
     position: absolute;
     width: 100%;
     height: 100%;
@@ -302,12 +398,7 @@ UI.meta = {
 .WIMF-main.dragging::after {
     opacity: .5;
 }
-.WIMF-title {
-    display: block;
-    font-size: 12px;
-    text-align: center;
-    transform: scale(.9)
-}
+
 .WIMF-button {
     display: inline-block;
     width: 17px;
@@ -340,19 +431,25 @@ UI.meta = {
     background-color: #fff;
     box-shadow: 0 0 4px #aaa;
 }
-.WIMF-mark {
-    background-color: #ffff81;
-}
+
 .WIMF-text {
+    position: absolute;
     display: none;
     top: 85px;
     right: 0;
-    width: 100px;
     height: 300px;
+
+    overflow: -moz-scrollbars-none;
+    overflow-y: scroll;
+    -ms-overflow-style: none;
+}
+.WIMF-text::-webkit-scrollbar {
+    display: none;
 }
 .WIMF-text a {
     overflow-wrap: anywhere;
 }
+
 .WIMF-text input {
     width: 95px;
     margin: 3px 0;
@@ -362,6 +459,7 @@ UI.meta = {
     outline: none;
     box-shadow: 0 0 3px #aaa;
 }
+
 .WIMF-text button {
     margin: 3px 0;
     padding: 0 5px;
@@ -377,6 +475,11 @@ UI.meta = {
 .WIMF-text button:hover {
     background-color: #bbb;
 }
+
+.WIMF-task p {
+    margin-bottom: 3px;
+    background-color: #9f9;
+}
 `
 }
 UI.M = new Proxy(UI.meta, {
@@ -391,14 +494,20 @@ UI.$btn = n => $(`.WIMF-button[name^=${n}]`)
 UI.action = {
     mark() {
         const $b = UI.$btn("mark")
-        if ($b.is(".active")) $(".WIMF-mark").removeClass("WIMF-mark")
-        else scan({
-            hl: $i => $i.addClass("WIMF-mark")
-        })
+        if ($b.is(".active")) {
+            $(".WIMF-mark").removeClass("WIMF-mark")
+            UI.task("表单高亮已取消。", "Form highlight is canceled.")
+        }
+        else {
+            scan({
+                hl: $i => $i.addClass("WIMF-mark")
+            })
+            UI.task("表单已高亮。", "Forms are highlighted.")
+        }
         $b.toggleClass("active")
     },
     fill() {
-        for (let o of ls.op) {
+        let c = 0; for (let o of ls.op) {
             const $i = $(o.path)
             if (! $i.length) Throw("Form path not found")
             switch (o.type) {
@@ -415,10 +524,13 @@ UI.action = {
                 default:
                     Throw("Unknown form type.")
             }
+            c++
         }
+        UI.task(`已填充 ${c} 个表单项。`, `${c} form field(s) is filled.`)
     },
     rset() {
         ls.op = []
+        UI.task("保存的表单已清除。", "Saved form is reset.")
     },
     conf() {
         UI.text.show("conf")
@@ -427,17 +539,30 @@ UI.action = {
         for (let i = 0; i < $A.length; i++) {
             const $b = $($A[i]),
                   zone = $b.data("zone"),
-                  $t = $b.prevAll(`input[name^=${zone}_]`)
-            for (let j = 0; j < $t.length; j++) {
-                const $e = $($t[j])
+                  $t = $b.prevAll(`input[type=text][name^=${zone}_]`),
+                  c_b = {
+                      sc: shortcut
+                  }
+
+            function map(it) {
+                for (let j = $t.length - 1; j >= 0; j--) {
+                    const $e = $($t[j]), sp = $e.attr("name").replace("_", ".")
+                    it($e, sp)
+                }
             }
+            map(($_, sp) => $_.val(ts._[sp]))
+            $b.on("click", () => {
+                map(($_, sp) => ts._[sp] = $_.val())
+                if (c_b[zone]) c_b[zone]()
+                UI.task(`设置块 ${zone} 已应用。`, `Configuration zone ${zone} is applied.`)
+            })
         }
     },
     info() {
         UI.text.show("info")
     },
     quit() {
-        $(".WIMF-main").hide()
+        $(".WIMF").melt("fadeout", 1.5, true)
     },
     back() {
         $(".WIMF-text").hide()
@@ -455,6 +580,7 @@ UI.text = {
         UI.$btn("quit").attr("name", "back 返回")
     }
 }
+UI.task = (m) => $(`<p>${m}</p>`).prependTo($(".WIMF-task")).melt("sudden", 3, true)
 UI.move = (t, r) => {
     if (t != null) ts.top = Math.max(t, 0)
     if (r != null) ts.right = Math.max(r, 0)
@@ -467,8 +593,9 @@ UI.init = () => {
 
     const $m = $(".WIMF-main"), $w = $(unsafeWindow)
 
-    $(".WIMF-button").on("click", e =>
-         UI.action[e.target.getAttribute("name").split(" ")[0]]())
+    $(".WIMF-button").on("click", function() {
+         UI.action[$(this).attr("name").split(" ")[0]]()
+    })
 
     $m.on("mousedown", e => {
         const { clientX: x0, clientY: y0 } = e
@@ -495,19 +622,26 @@ UI.init = () => {
     })
 }
 
-$(() => {
+$(function init() {
     ls({
         op: []
     })
     ts({
         top: 0,
         right: 0,
-        key: {
-            leader: "A+w",
-            toggle: "L>q"
+        sc: {
+            leader: "Alt-w",
+            toggle: "&q",
+            mark: "&m",
+            fill: "&f",
+            rset: "&r",
+            conf: "&c",
+            info: "&i"
         }
     })
+
     UI.init()
     scan()
+    shortcut()
 })
 
